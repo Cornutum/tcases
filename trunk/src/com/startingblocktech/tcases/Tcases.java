ae8e5d2d325a09daccd3270ae334913c7d2d355b
kerrykimbrough <kerrykimbrough@43bf6a5c-2e60-391d-9967-45c4e5fce2ab>
2010-05-23 23:35:28 +0000

    Initial version

---
+++ trunk/src/com/startingblocktech/tcases/Tcases.java
@@ -0,0 +1,68 @@
+//////////////////////////////////////////////////////////////////////////////
+// 
+//               Copyright 2010, Starting Block Technologies
+//                        www.startingblocktech.com
+//                           All Rights Reserved
+//
+//////////////////////////////////////////////////////////////////////////////
+
+package com.startingblocktech.tcases;
+
+import com.startingblocktech.tcases.io.SystemInputDocReader;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.FileInputStream;
+
+/**
+ * Generates a set of {@link TestCase test cases} from a {@link SystemInputDef system input definition}.
+ *
+ * @version $Revision$, $Date$
+ */
+public class Tcases
+  {
+  /**
+   * Creates a new Tcases object.
+   */
+  public Tcases()
+    {
+    }
+
+  /**
+   * Generates a set of {@link TestCase test cases} from a {@link SystemInputDef system input definition}.
+   */
+  public static void main( String[] args)
+    {
+    int exitCode = 0;
+    try
+      {
+      String file = args[0];
+      
+      FileInputStream stream = null;
+      try
+        {
+        stream = new FileInputStream( file);
+        SystemInputDocReader reader = new SystemInputDocReader( stream);
+        SystemInputDef inputDef = reader.getSystemInputDef();
+        }
+      catch( Exception e)
+        {
+        throw new RuntimeException( "Can't read file=" + file, e);
+        }
+      finally
+        {
+        IOUtils.closeQuietly( stream);
+        }
+      }
+    catch( Exception e)
+      {
+      exitCode = 1;
+      e.printStackTrace();
+      }
+    finally
+      {
+      System.exit( exitCode);
+      }
+    }
+  }
+
