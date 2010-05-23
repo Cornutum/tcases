3ea104022e6e8d297f254ec4ecadfbc04f8284e8
kerrykimbrough <kerrykimbrough@43bf6a5c-2e60-391d-9967-45c4e5fce2ab>
2010-05-23 23:36:38 +0000

    Initial version

---
+++ src/com/startingblocktech/tcases/generator/ITestCaseGenerator.java
@@ -0,0 +1,30 @@
+//////////////////////////////////////////////////////////////////////////////
+// 
+//               Copyright 2010, Starting Block Technologies
+//                        www.startingblocktech.com
+//
+//////////////////////////////////////////////////////////////////////////////
+
+package com.startingblocktech.tcases.generator;
+
+import com.startingblocktech.tcases.*;
+
+/**
+ * Generates {@link TestCase test cases} for a {@link FunctionInputDef function}.
+ *
+ * @version $Revision$, $Date$
+ */
+public interface ITestCaseGenerator
+  {
+  /**
+   * Changes the random number sequence seed for this generator.
+   */
+  void setRandomSeed( long seed);
+
+  /**
+   * Returns a set of {@link TestCase test cases} for the given function input definition.
+   * If the given base test definition is non-null, returns a set of new test cases
+   * that extend the base tests.
+   */
+  FunctionTestDef getTests( FunctionInputDef inputDef, FunctionTestDef baseTests);  
+  }
