69731d9c459598c2a3af153e307a9d259bb8bed5
kerrykimbrough <kerrykimbrough@43bf6a5c-2e60-391d-9967-45c4e5fce2ab>
2010-06-20 21:34:10 +0000

    Refator using Tuple

---
+++ trunk/src/com/startingblocktech/tcases/generator/Tuple.java
@@ -0,0 +1,144 @@
+//////////////////////////////////////////////////////////////////////////////
+// 
+//               Copyright 2010, Starting Block Technologies
+//                        www.startingblocktech.com
+//
+//////////////////////////////////////////////////////////////////////////////
+
+package com.startingblocktech.tcases.generator;
+
+import com.startingblocktech.tcases.VarDef;
+import com.startingblocktech.tcases.util.ToString;
+
+import org.apache.commons.lang.ObjectUtils;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * Represents a set of {@link VarBindingDef input variable bindings}.
+ *
+ * @version $Revision$, $Date$
+ */
+public class Tuple
+  {
+  /**
+   * Creates a new Tuple object.
+   */
+  public Tuple()
+    {
+    this( (Collection<VarBindingDef>) null);
+    }
+  
+  /**
+   * Creates a new Tuple object.
+   */
+  public Tuple( VarBindingDef ... bindings)
+    {
+    setBindings( Arrays.asList( bindings));
+    }
+  
+  /**
+   * Creates a new Tuple object.
+   */
+  public Tuple( Collection<VarBindingDef> bindings)
+    {
+    setBindings( bindings);
+    }
+
+  /**
+   * Changes the variable bindings for this tuple.
+   */
+  public void setBindings( Collection<VarBindingDef> bindings)
+    {
+    bindings_ = new HashMap<VarDef,VarBindingDef>();
+    if( bindings != null)
+      {
+      for( VarBindingDef binding : bindings)
+        {
+        add( binding);
+        }
+      }
+    }
+
+  /**
+   * Returns the variable bindings for this tuple.
+   */
+  public Iterator< VarBindingDef> getBindings()
+    {
+    return bindings_.values().iterator();
+    }
+
+  /**
+   * Adds a binding to this tuple.
+   */
+  public Tuple add( VarBindingDef binding)
+    {
+    remove( binding.getVarDef());
+    bindings_.put( binding.getVarDef(), binding);
+
+    return this;
+    }
+
+  /**
+   * Adds all bindings from the given tuple.
+   */
+  public Tuple addAll( Tuple tuple)
+    {
+    for( Iterator<VarBindingDef> bindings = tuple.getBindings(); bindings.hasNext();)
+      {
+      add( bindings.next());
+      }
+
+    return this;
+    }
+
+  /**
+   * Removes a binding from this tuple.
+   */
+  public Tuple remove( VarDef var)
+    {
+    VarBindingDef binding = bindings_.remove( var);
+    if( binding != null)
+      {
+      }
+
+    return this;
+    }
+
+  public boolean equals( Object object)
+    {
+    Tuple other =
+      object != null && object.getClass().equals( getClass())
+      ? (Tuple) object
+      : null;
+
+    return
+      other != null
+      && ObjectUtils.equals( other.bindings_, bindings_);
+    }
+
+  public int hashCode()
+    {
+    return
+      getClass().hashCode()
+      ^ (bindings_.hashCode());
+    }
+
+  public String toString()
+    {
+    ToStringBuilder builder = ToString.getBuilder( this);
+    if( !bindings_.isEmpty())
+      {
+      builder.append( bindings_.values());
+      }
+    return builder.toString();
+    }
+
+  private Map<VarDef,VarBindingDef> bindings_;
+  }
+
