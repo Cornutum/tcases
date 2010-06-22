95778c5c6cbf92ce7a105f7c242434e299db918c
kerrykimbrough <kerrykimbrough@43bf6a5c-2e60-391d-9967-45c4e5fce2ab>
2010-06-22 03:00:21 +0000

    Add testAllPairsCompatible()

--- test/com/startingblocktech/tcases/generator/TestTupleIterator.java
+++ test/com/startingblocktech/tcases/generator/TestTupleIterator.java
@@ -9,6 +9,7 @@ package com.startingblocktech.tcases.generator;
 
 import com.startingblocktech.tcases.VarDef;
 import com.startingblocktech.tcases.VarValueDef;
+import com.startingblocktech.tcases.conditions.*;
 import com.startingblocktech.tcases.util.Asserts;
 
 import org.junit.Test;
@@ -147,6 +148,76 @@ public class TestTupleIterator
     expectedTuples.add
       ( new Tuple
         ( new VarBindingDef( var2, value22),
+          new VarBindingDef( var3, value31)));
+    expectedTuples.add
+      ( new Tuple
+        ( new VarBindingDef( var2, value21),
+          new VarBindingDef( var3, value32)));
+    expectedTuples.add
+      ( new Tuple
+        ( new VarBindingDef( var2, value22),
+          new VarBindingDef( var3, value32)));
+
+    Asserts.assertSeqEquals( "Tuples", expectedTuples, tuples);
+    }
+  
+  @Test
+  public void testAllPairsCompatible()
+    {
+    // Given...
+    VarValueDef value11 = new VarValueDef( "value-1-1").addProperties( "R1", "V1");
+    VarValueDef value12 = new VarValueDef( "value-1-2").addProperties( "R1", "V2");
+    VarDef      var1    = new VarDef( "var-1").addValue( value11).addValue( value12);
+
+    VarValueDef value21 = new VarValueDef( "value-2-1").addProperties( "R2", "V1");
+    VarValueDef value22 = new VarValueDef( "value-2-2").addProperties( "R2", "V2");
+    VarDef      var2    = new VarDef( "var-2").addValue( value21).addValue( value22);
+
+    VarValueDef value31 = new VarValueDef( "value-3-1").addProperties( "R3", "V1");
+    VarValueDef value32 = new VarValueDef( "value-3-2").addProperties( "R3", "V2");
+    VarDef      var3    = new VarDef( "var-3").addValue( value31).addValue( value32);
+
+    value11.setCondition( new Not( new ContainsAny( "R2")));
+    value22.setCondition( new AllOf( new ContainsAny( "U1"), new Not( new ContainsAll( "R3", "V1"))));
+    value31.setCondition( new AnyOf( new Not( new ContainsAny( "R1")), new Not( new ContainsAll( "R2", "R3"))));
+    
+    List<VarDef> varDefs = new ArrayList<VarDef>();
+    varDefs.add( var1);
+    varDefs.add( var2);
+    varDefs.add( var3);
+    
+    // When...
+    TupleIterator tuples = new TupleIterator( 2, varDefs);
+
+    // Then...
+    List<Tuple> expectedTuples = new ArrayList<Tuple>();
+    expectedTuples.add
+      ( new Tuple
+        ( new VarBindingDef( var1, value12),
+          new VarBindingDef( var2, value21)));
+    expectedTuples.add
+      ( new Tuple
+        ( new VarBindingDef( var1, value12),
+          new VarBindingDef( var2, value22)));
+    expectedTuples.add
+      ( new Tuple
+        ( new VarBindingDef( var1, value11),
+          new VarBindingDef( var3, value31)));
+    expectedTuples.add
+      ( new Tuple
+        ( new VarBindingDef( var1, value12),
+          new VarBindingDef( var3, value31)));
+    expectedTuples.add
+      ( new Tuple
+        ( new VarBindingDef( var1, value11),
+          new VarBindingDef( var3, value32)));
+    expectedTuples.add
+      ( new Tuple
+        ( new VarBindingDef( var1, value12),
+          new VarBindingDef( var3, value32)));
+    expectedTuples.add
+      ( new Tuple
+        ( new VarBindingDef( var2, value21),
           new VarBindingDef( var3, value31)));
     expectedTuples.add
       ( new Tuple
