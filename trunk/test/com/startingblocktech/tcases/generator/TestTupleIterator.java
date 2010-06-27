86b2047fbd764e6e8c27a84a97084f0a6657bffc
kerrykimbrough <kerrykimbrough@43bf6a5c-2e60-391d-9967-45c4e5fce2ab>
2010-06-27 01:55:31 +0000

    TupleIterator: if any variable has no no valid values, can't complete specified tuples

--- trunk/test/com/startingblocktech/tcases/generator/TestTupleIterator.java
+++ trunk/test/com/startingblocktech/tcases/generator/TestTupleIterator.java
@@ -33,52 +33,45 @@ public class TestTupleIterator
     VarValueDef value12 = new VarValueDef( "value-1-2");
     VarValueDef value13 = new VarValueDef( "value-1-3", VarValueDef.Type.FAILURE);
     VarDef      var1    = new VarDef( "var-1").addValue( value11).addValue( value12).addValue( value13);
-    
-    VarDef var2 = new VarDef( "var-2");
 
-    VarValueDef value31 = new VarValueDef( "value-3-1", VarValueDef.Type.FAILURE);
+    VarValueDef value21 = new VarValueDef( "value-2-1", VarValueDef.Type.FAILURE);
+    VarValueDef value22 = new VarValueDef( "value-2-2");
+    VarDef var2 = new VarDef( "var-2").addValue( value21).addValue( value22);
+
+    VarValueDef value31 = new VarValueDef( "value-3-1");
     VarValueDef value32 = new VarValueDef( "value-3-2");
     VarDef var3 = new VarDef( "var-3").addValue( value31).addValue( value32);
-    
-    VarValueDef value41 = new VarValueDef( "value-4-1", VarValueDef.Type.FAILURE);
-    VarDef var4 = new VarDef( "var-4").addValue( value41);
-
-    VarValueDef value51 = new VarValueDef( "value-5-1");
-    VarValueDef value52 = new VarValueDef( "value-5-2");
-    VarDef var5 = new VarDef( "var-5").addValue( value51).addValue( value52);
     
     List<VarDef> varDefs = new ArrayList<VarDef>();
     varDefs.add( var1);
     varDefs.add( var2);
     varDefs.add( var3);
-    varDefs.add( var4);
-    varDefs.add( var5);
     
     // When...
-    TupleIterator tuples = new TupleIterator( 5, varDefs);
+    TupleIterator tuples = new TupleIterator( 3, varDefs);
 
     // Then...
     List<Tuple> expectedTuples = new ArrayList<Tuple>();
     expectedTuples.add
       ( new Tuple
         ( new VarBindingDef( var1, value11),
-          new VarBindingDef( var3, value32),
-          new VarBindingDef( var5, value51)));
+          new VarBindingDef( var2, value22),
+          new VarBindingDef( var3, value31)));
     expectedTuples.add
       ( new Tuple
         ( new VarBindingDef( var1, value12),
-          new VarBindingDef( var3, value32),
-          new VarBindingDef( var5, value51)));
+          new VarBindingDef( var2, value22),
+          new VarBindingDef( var3, value31)));
     expectedTuples.add
       ( new Tuple
         ( new VarBindingDef( var1, value11),
-          new VarBindingDef( var3, value32),
-          new VarBindingDef( var5, value52)));
+          new VarBindingDef( var2, value22),
+          new VarBindingDef( var3, value32)));
     expectedTuples.add
       ( new Tuple
         ( new VarBindingDef( var1, value12),
-          new VarBindingDef( var3, value32),
-          new VarBindingDef( var5, value52)));
+          new VarBindingDef( var2, value22),
+          new VarBindingDef( var3, value32)));
 
     Asserts.assertSeqEquals( "Tuples", expectedTuples, tuples);
     }
@@ -333,7 +326,6 @@ public class TestTupleIterator
       {
       throw new RuntimeException( "Unexpected exception", e);
       }
-
     }
 
   @Test
@@ -359,7 +351,31 @@ public class TestTupleIterator
       {
       throw new RuntimeException( "Unexpected exception", e);
       }
+    }
 
+  @Test
+  public void testValidValues()
+    {
+    // Given...
+    List<VarDef> varDefs = new ArrayList<VarDef>();
+    varDefs.add( new VarDef( "var-1").addValue( new VarValueDef( "value-1-1")));
+    varDefs.add( new VarDef( "var-2").addValue( new VarValueDef( "value-2-1", VarValueDef.Type.FAILURE)));
+    
+    // When...
+    try
+      {
+      TupleIterator tuples = new TupleIterator( 2, varDefs);
+      Asserts.assertSeqEquals( "Tuples", (List<Tuple>)null, tuples);
+      }
+
+    // Then...
+    catch( IllegalStateException ise)
+      {
+      }
+    catch( Exception e)
+      {
+      throw new RuntimeException( "Unexpected exception", e);
+      }
     }
   }
 
