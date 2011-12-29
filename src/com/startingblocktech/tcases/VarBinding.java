b262d02e7effa23c7c43b8cda8ac6b8e138ec321
kerrykimbrough@gmail.com <kerrykimbrough@gmail.com@43bf6a5c-2e60-391d-9967-45c4e5fce2ab>
2011-12-29 22:06:29 +0000

    Add support for equals, compareTo

--- src/com/startingblocktech/tcases/VarBinding.java
+++ src/com/startingblocktech/tcases/VarBinding.java
@@ -10,6 +10,7 @@ package com.startingblocktech.tcases;
 import com.startingblocktech.tcases.util.ToString;
 import static com.startingblocktech.tcases.DefUtils.*;
 
+import org.apache.commons.lang.ObjectUtils;
 import org.apache.commons.lang.builder.ToStringBuilder;
 
 /**
@@ -17,7 +18,7 @@ import org.apache.commons.lang.builder.ToStringBuilder;
  *
  * @version $Revision$, $Date$
  */
-public class VarBinding
+public class VarBinding implements Comparable<VarBinding>
   {
   /**
    * Creates a new VarBinding object.
@@ -145,6 +146,36 @@ public class VarBinding
       }
 
     return builder.toString();
+    }
+
+  public int compareTo( VarBinding other)
+    {
+    return getVar().compareTo( other.getVar());
+    }
+
+  public int hashCode()
+    {
+    return
+      getClass().hashCode()
+      ^ (getVar()==null? 0 : getVar().hashCode())
+      ^ (getType()==null? 0 : getType().hashCode())
+      ^ (getValue()==null? 0 : getValue().hashCode())
+      ^ (isValueValid()? Boolean.TRUE.hashCode() : Boolean.FALSE.hashCode());
+    }
+
+  public boolean equals( Object object)
+    {
+    VarBinding other =
+      object != null && object.getClass().equals( getClass())
+      ? (VarBinding) object
+      : null;
+
+    return
+      other != null
+      && ObjectUtils.equals( getVar(), other.getVar())
+      && ObjectUtils.equals( getType(), other.getType())
+      && ObjectUtils.equals( getValue(), other.getValue())
+      && isValueValid() == other.isValueValid();
     }
 
   private String var_;
