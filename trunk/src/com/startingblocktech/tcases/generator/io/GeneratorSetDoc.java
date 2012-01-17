db4da47436f69d7448046432a1189f99cbe3be62
kerrykimbrough@gmail.com <kerrykimbrough@gmail.com@43bf6a5c-2e60-391d-9967-45c4e5fce2ab>
2012-01-17 20:54:03 +0000

    GeneratorSetDocReader: Refactor using TupleGeneratorDoc, GeneratorSetDoc

---
+++ trunk/src/com/startingblocktech/tcases/generator/io/GeneratorSetDoc.java
@@ -0,0 +1,23 @@
+//////////////////////////////////////////////////////////////////////////////
+// 
+//               Copyright 2010, Starting Block Technologies
+//                        www.startingblocktech.com
+//
+//////////////////////////////////////////////////////////////////////////////
+
+package com.startingblocktech.tcases.generator.io;
+
+
+/**
+ * Defines constants for the identifiers in a generator definition document.
+ *
+ * @version $Revision$, $Date$
+ */
+public final class GeneratorSetDoc
+  {
+  private GeneratorSetDoc()
+    {
+    }
+
+  public static final String GENERATORS_TAG      = "Generators";
+  }
