797bb93c07e8658abb6c2f430e60c98abe4216a4
kerrykimbrough@gmail.com <kerrykimbrough@gmail.com@43bf6a5c-2e60-391d-9967-45c4e5fce2ab>
2012-01-31 23:43:49 +0000

    Add testWithParams

--- trunk/test/com/startingblocktech/tcases/io/TestTransformFilter.java
+++ trunk/test/com/startingblocktech/tcases/io/TestTransformFilter.java
@@ -10,9 +10,12 @@ package com.startingblocktech.tcases.io;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.regex.Pattern;
 
 /**
  * Runs tests for the {@link TransformFilter}.
@@ -40,6 +43,65 @@ public class TestTransformFilter
     filter.getSource().close();
     
     // Then...
-    assertEquals( "Output written, target=" + target, true, target.length() > 0);
+    assertEquals( "Output written", true, target.length() > 0);
+
+    String result = FileUtils.readFileToString( target);
+    assertEquals
+      ( "System tested=default",
+        true,
+        contains( result, "Tests \\{@link System-2#Function-0 Function-0\\(\\)\\}"));
+    assertEquals
+      ( "Throws=default",
+        true,
+        contains( result, "public void Function-0_0\\(\\)$"));
+    }
+  
+  @Test
+  public void testWithParams() throws Exception
+    {
+    // Given...
+    TransformFilter filter =
+      new TransformFilter
+      ( getClass().getResourceAsStream( "/testDef2Junit.xsl"));
+
+    File target = File.createTempFile( "TestTransformFilter-WithParams-", ".java");
+    target.deleteOnExit();
+    filter.setTarget( target);
+
+    HashMap<String,Object> params = new HashMap<String,Object>();
+    params.put( "throws", "true");
+    params.put( "class", "SUT");
+    filter.setParams( params);
+
+    // When...
+    IOUtils.copy
+      ( getClass().getResourceAsStream( "system-test-def-2.xml"),
+        filter.getSource());
+    filter.getSource().close();
+    
+    // Then...
+    assertEquals( "Output written", true, target.length() > 0);
+
+    String result = FileUtils.readFileToString( target);
+    assertEquals
+      ( "System tested=SUT",
+        true,
+        contains( result, "Tests \\{@link SUT#Function-0 Function-0\\(\\)\\}"));
+    assertEquals
+      ( "Throws=Exception",
+        true,
+        contains( result, "public void Function-0_0\\(\\) throws Exception$"));
+    }
+
+  /**
+   * Returns true if the given transform result contains a match for the given
+   * regular expression.
+   */
+  private boolean contains( String result, String pattern)
+    {
+    return
+      Pattern.compile( pattern, Pattern.MULTILINE)
+      .matcher( result)
+      .find();
     }
   }
