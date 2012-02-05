8701cba5eacd67df71ce456842a9d87c20eb8176
kerrykimbrough <kerrykimbrough@43bf6a5c-2e60-391d-9967-45c4e5fce2ab>
2012-02-05 23:00:11 +0000

    Add convertTautology

--- test/com/startingblocktech/tcases/conditions/TestCnf.java
+++ test/com/startingblocktech/tcases/conditions/TestCnf.java
@@ -105,6 +105,54 @@ public class TestCnf
     assertEquals( "Satisfied: !A,  B, !C", true, conjunct.satisfied( new PropertySet( "B")));
     assertEquals( "Satisfied: !A, !B, !C", false, conjunct.satisfied( new PropertySet()));
     }
+  
+  @Test
+  public void convertTautology()
+    {
+    // Given...
+    ICondition condition =
+      new AnyOf()
+      .add( new AllOf()
+            .add( new ContainsAll( "A"))
+            .add( new AnyOf()
+                  .add( new ContainsAny( "B"))
+                  .add( new Not().add( new ContainsAny( "C")))))
+      .add( new Not()
+            .add( new AnyOf()
+                  .add( new ContainsAll( "A"))
+                  .add( new AllOf()
+                        .add( new Not().add( new ContainsAll( "B")))
+                        .add( new ContainsAny( "C")))));
+      
+    // When...
+    IConjunct conjunct = Cnf.convert( condition);
+    
+    // Then...
+    assertEquals( "Satisfied:  A,  B,  C", true, conjunct.satisfied( new PropertySet( "A", "B", "C")));
+    assertEquals( "Satisfied:  A,  B, !C", true, conjunct.satisfied( new PropertySet( "A", "B")));
+    assertEquals( "Satisfied:  A, !B,  C", false, conjunct.satisfied( new PropertySet( "A", "C")));
+    assertEquals( "Satisfied:  A, !B, !C", true, conjunct.satisfied( new PropertySet( "A")));
+    assertEquals( "Satisfied: !A,  B,  C", true, conjunct.satisfied( new PropertySet( "B", "C")));
+    assertEquals( "Satisfied: !A, !B,  C", false, conjunct.satisfied( new PropertySet( "C")));
+    assertEquals( "Satisfied: !A,  B, !C", true, conjunct.satisfied( new PropertySet( "B")));
+    assertEquals( "Satisfied: !A, !B, !C", true, conjunct.satisfied( new PropertySet()));
+
+    // Given...
+    condition = new Not( condition);
+      
+    // When...
+    conjunct = Cnf.convert( condition);
+    
+    // Then...
+    assertEquals( "Satisfied:  A,  B,  C", false, conjunct.satisfied( new PropertySet( "A", "B", "C")));
+    assertEquals( "Satisfied:  A,  B, !C", false, conjunct.satisfied( new PropertySet( "A", "B")));
+    assertEquals( "Satisfied:  A, !B,  C", true, conjunct.satisfied( new PropertySet( "A", "C")));
+    assertEquals( "Satisfied:  A, !B, !C", false, conjunct.satisfied( new PropertySet( "A")));
+    assertEquals( "Satisfied: !A,  B,  C", false, conjunct.satisfied( new PropertySet( "B", "C")));
+    assertEquals( "Satisfied: !A, !B,  C", true, conjunct.satisfied( new PropertySet( "C")));
+    assertEquals( "Satisfied: !A,  B, !C", false, conjunct.satisfied( new PropertySet( "B")));
+    assertEquals( "Satisfied: !A, !B, !C", false, conjunct.satisfied( new PropertySet()));
+    }
 
   }
 
