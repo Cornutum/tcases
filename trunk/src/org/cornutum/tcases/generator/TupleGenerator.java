8adc69aa61b611aa0432a3f8ff38f2f109573cbc
kerrykimbrough <kerrykimbrough@43bf6a5c-2e60-391d-9967-45c4e5fce2ab>
2013-01-14 01:37:17 +0000

    Remove unused imports.

--- trunk/src/org/cornutum/tcases/generator/TupleGenerator.java
+++ trunk/src/org/cornutum/tcases/generator/TupleGenerator.java
@@ -21,10 +21,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 
 /**
  * Generates {@link TestCase test cases} for a {@link FunctionInputDef function} that use
