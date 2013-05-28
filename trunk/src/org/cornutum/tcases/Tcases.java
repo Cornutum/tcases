905df40ce8d32fbb502bb71237a1b4ded7f70c8b
kerrykimbrough@gmail.com <kerrykimbrough@gmail.com@43bf6a5c-2e60-391d-9967-45c4e5fce2ab>
2013-05-28 21:36:27 +0000

    Release=1.0.1

--- trunk/src/org/cornutum/tcases/Tcases.java
+++ trunk/src/org/cornutum/tcases/Tcases.java
@@ -1163,7 +1163,7 @@ public class Tcases
       + ")";
     }
 
-  public static final String VERSION = "1.0.0";
+  public static final String VERSION = "1.0.1";
   public static final String VERSION_DATE = "$Date$";
   public static final String VERSION_REV = "$Revision$";
   private static final Logger logger_ = LoggerFactory.getLogger( Tcases.class);
