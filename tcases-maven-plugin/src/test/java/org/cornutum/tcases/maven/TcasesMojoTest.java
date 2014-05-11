df79cc8ae69f176813e46e1ff63bfcb6242cdc0b
kerrykimbrough <kerrykimbrough@43bf6a5c-2e60-391d-9967-45c4e5fce2ab>
2014-05-11 00:11:20 +0000

    Complete all test cases

--- tcases-maven-plugin/src/test/java/org/cornutum/tcases/maven/TcasesMojoTest.java
+++ tcases-maven-plugin/src/test/java/org/cornutum/tcases/maven/TcasesMojoTest.java
@@ -2,6 +2,7 @@ package org.cornutum.tcases.maven;
 
 import org.cornutum.tcases.Tcases;
 
+import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.testing.MojoRule;
 import org.codehaus.plexus.PlexusTestCase;
 import org.codehaus.plexus.util.DirectoryScanner;
@@ -11,6 +12,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 import java.io.File;
+import java.util.Arrays;
 
 /**
  * Runs tests for the {@link TcaseMojo} plugin.
@@ -154,27 +156,31 @@ public class TcasesMojoTest
    * </P>
    */
   @Test
-  public void whenInputPatternsNone() throws Exception
+  public void whenOutputJUnit() throws Exception
     {
     // Given...
-    File baseDirTest = getBaseDirTest( "tcases-project-none");
+    File baseDirTest = getBaseDirTest( "tcases-project-junit");
 
     // When...
     TcasesMojo tcasesMojo = (TcasesMojo) mojoHelper.lookupConfiguredMojo( baseDirTest, "tcases");
     tcasesMojo.execute();
 
     // Then...
-    File expectedInputDir = new File( baseDirTest, "tcases/input");
+    File expectedInputDir = new File( baseDirTest, "src/test/tcases");
     assertEquals( "Input dir", expectedInputDir, tcasesMojo.getInputDirFile());
 
-    File expectedOutDir = new File( baseDirTest, "target/tcases/output");
+    File expectedOutDir = new File( baseDirTest, "target/tcases");
     assertEquals( "Out dir", expectedOutDir, tcasesMojo.getOutDirFile());
 
-    String[] expectedInputDefs = findPathsMatching( expectedInputDir, "Input.xml", "*/The*Input.xml", "**/Other.xml");
-    assertEquals( "Input defs", 3, expectedInputDefs.length);
+    String[] expectedInputDefs = findPathsMatching( expectedInputDir, "**/*-Input.xml");
+    assertEquals( "Input defs", 1, expectedInputDefs.length);
 
     String[] expectedTestDefs = findPathsMatching( expectedOutDir, "**/*");
-    assertEquals( "Test defs", 3, expectedTestDefs.length);
+    assertEquals( "Test defs", 1, expectedTestDefs.length);
+
+    File expectedInputDef = new File( expectedInputDefs[0]);
+    File expectedTestDef = new File( expectedInputDef.getParent(), Tcases.getProjectName( expectedInputDef) + "Test.java");
+    assertEquals( "Test def", expectedTestDef.getPath(), expectedTestDefs[0]);
     }
 
   /**
@@ -208,13 +214,36 @@ public class TcasesMojoTest
    * </P>
    */
   @Test
-  public void execute_2()
+  public void whenInputPatternsOne() throws Exception
     {
     // Given...
+    File baseDirTest = getBaseDirTest( "tcases-project-one");
 
     // When...
+    TcasesMojo tcasesMojo = (TcasesMojo) mojoHelper.lookupConfiguredMojo( baseDirTest, "tcases");
+    tcasesMojo.execute();
 
     // Then...
+    File expectedInputDir = new File( baseDirTest, "src/test/tcases");
+    assertEquals( "Input dir", expectedInputDir, tcasesMojo.getInputDirFile());
+
+    File expectedOutDir = new File( baseDirTest, "target/tcases");
+    assertEquals( "Out dir", expectedOutDir, tcasesMojo.getOutDirFile());
+
+    String[] expectedInputDefs = findPathsMatching( expectedInputDir, "**/Project-*-Inputs.xml");
+    assertEquals( "Input defs", 3, expectedInputDefs.length);
+
+    String[] expectedTestDefs = findPathsMatching( expectedOutDir, "**/*");
+    assertEquals( "Test defs", 3, expectedTestDefs.length);
+
+    Arrays.sort( expectedInputDefs);
+    Arrays.sort( expectedTestDefs);
+    for( int i = 0; i < expectedInputDefs.length; i++)
+      {
+      File expectedInputDef = new File( expectedInputDefs[i]);
+      File expectedTestDef = new File( expectedInputDef.getParent(), "Tests-For-" + Tcases.getProjectName( expectedInputDef) + ".xml");
+      assertEquals( "Test def", expectedTestDef.getPath(), expectedTestDefs[i]);
+      }
     }
 
   /**
@@ -248,13 +277,23 @@ public class TcasesMojoTest
    * </P>
    */
   @Test
-  public void execute_3()
+  public void whenInputDefsNone() throws Exception
     {
     // Given...
+    File baseDirTest = getBaseDirTest( "tcases-project-none");
 
     // When...
+    TcasesMojo tcasesMojo = (TcasesMojo) mojoHelper.lookupConfiguredMojo( baseDirTest, "tcases");
+    tcasesMojo.execute();
 
     // Then...
+    File expectedInputDir = new File( baseDirTest, "src/test/tcases");
+    assertEquals( "Input dir", expectedInputDir, tcasesMojo.getInputDirFile());
+
+    File expectedOutDir = new File( baseDirTest, "target/tcases");
+    assertEquals( "Out dir", expectedOutDir, tcasesMojo.getOutDirFile());
+
+    assertEquals( "Out dir created", false, expectedOutDir.exists());
     }
 
   /**
@@ -288,13 +327,31 @@ public class TcasesMojoTest
    * </P>
    */
   @Test
-  public void execute_4()
+  public void whenOutputNew() throws Exception
     {
     // Given...
+    File baseDirTest = getBaseDirTest( "tcases-project-new");
 
     // When...
+    TcasesMojo tcasesMojo = (TcasesMojo) mojoHelper.lookupConfiguredMojo( baseDirTest, "tcases");
+    tcasesMojo.execute();
 
     // Then...
+    File expectedInputDir = new File( baseDirTest, "src/test/tcases");
+    assertEquals( "Input dir", expectedInputDir, tcasesMojo.getInputDirFile());
+
+    File expectedOutDir = new File( baseDirTest, "target/tcases");
+    assertEquals( "Out dir", expectedOutDir, tcasesMojo.getOutDirFile());
+
+    String[] expectedInputDefs = findPathsMatching( expectedInputDir, "**/*-Input.xml");
+    assertEquals( "Input defs", 1, expectedInputDefs.length);
+
+    String[] expectedTestDefs = findPathsMatching( expectedOutDir, "**/*");
+    assertEquals( "Test defs", 1, expectedTestDefs.length);
+
+    File expectedInputDef = new File( expectedInputDefs[0]);
+    File expectedTestDef = new File( expectedInputDef.getParent(), "TestCases.xml");
+    assertEquals( "Test def", expectedTestDef.getPath(), expectedTestDefs[0]);
     }
 
   /**
@@ -303,38 +360,34 @@ public class TcasesMojoTest
    * <TABLE border="1" cellpadding="8">
    * <TR align="left"><TH colspan=2> 5. execute (<FONT color="red">Failure</FONT>) </TH></TR>
    * <TR align="left"><TH> Input Choice </TH> <TH> Value </TH></TR>
-   * <TR><TD> InputDefPatterns.Count </TD> <TD> One </TD> </TR>
-   * <TR><TD> InputDefPatterns.Matched </TD> <TD> Many </TD> </TR>
-   * <TR><TD> InputDir </TD> <TD> Default </TD> </TR>
-   * <TR><TD> OutDir </TD> <TD> Default </TD> </TR>
-   * <TR><TD> OutFile.Defined </TD> <TD> No </TD> </TR>
-   * <TR><TD> OutFile.Wildcard </TD> <TD> NA </TD> </TR>
-   * <TR><TD> TestDef.Defined </TD> <TD> Yes </TD> </TR>
    * <TR><TD> TestDef.Wildcard </TD> <TD> <FONT color="red"> Many  </FONT> </TD> </TR>
-   * <TR><TD> TestDef.Exists </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> GenDef.Defined </TD> <TD> No </TD> </TR>
-   * <TR><TD> GenDef.Wildcard </TD> <TD> NA </TD> </TR>
-   * <TR><TD> GenDef.Exists </TD> <TD> NA </TD> </TR>
-   * <TR><TD> TransformDef.Defined </TD> <TD> No </TD> </TR>
-   * <TR><TD> TransformDef.Relative </TD> <TD> NA </TD> </TR>
-   * <TR><TD> TransformDef.Wildcard </TD> <TD> NA </TD> </TR>
-   * <TR><TD> TransformDef.Exists </TD> <TD> NA </TD> </TR>
-   * <TR><TD> TransformParams.Defined </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> Junit </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> NewTests </TD> <TD> Default </TD> </TR>
-   * <TR><TD> Seed.Defined </TD> <TD> No </TD> </TR>
-   * <TR><TD> DefaultTupleSize.Defined </TD> <TD> No </TD> </TR>
    * </TABLE>
    * </P>
    */
   @Test
-  public void execute_5()
+  public void whenTestDefPatternInvalid() throws Exception
     {
     // Given...
+    File baseDirTest = getBaseDirTest( "tcases-project-default");
 
+    TcasesMojo tcasesMojo = (TcasesMojo) mojoHelper.lookupConfiguredMojo( baseDirTest, "tcases");
+    tcasesMojo.setTestDef( "*-Test-*.xml");
+
+    
     // When...
-
+    try
+      {
+      tcasesMojo.execute();
+      fail( "Expected failure missing");
+      }
     // Then...
+    catch( MojoExecutionException expected)
+      {
+      }
+    catch( Exception e)
+      {
+      throw new RuntimeException( "Unexpected failure", e);
+      }
     }
 
   /**
@@ -343,240 +396,36 @@ public class TcasesMojoTest
    * <TABLE border="1" cellpadding="8">
    * <TR align="left"><TH colspan=2> 6. execute (<FONT color="red">Failure</FONT>) </TH></TR>
    * <TR align="left"><TH> Input Choice </TH> <TH> Value </TH></TR>
-   * <TR><TD> InputDefPatterns.Count </TD> <TD> Many </TD> </TR>
-   * <TR><TD> InputDefPatterns.Matched </TD> <TD> One </TD> </TR>
-   * <TR><TD> InputDir </TD> <TD> Default </TD> </TR>
-   * <TR><TD> OutDir </TD> <TD> Default </TD> </TR>
-   * <TR><TD> OutFile.Defined </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> OutFile.Wildcard </TD> <TD> One </TD> </TR>
-   * <TR><TD> TestDef.Defined </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> TestDef.Wildcard </TD> <TD> One </TD> </TR>
-   * <TR><TD> TestDef.Exists </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> GenDef.Defined </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> GenDef.Wildcard </TD> <TD> One </TD> </TR>
-   * <TR><TD> GenDef.Exists </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> TransformDef.Defined </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> TransformDef.Relative </TD> <TD> No </TD> </TR>
-   * <TR><TD> TransformDef.Wildcard </TD> <TD> One </TD> </TR>
    * <TR><TD> TransformDef.Exists </TD> <TD> <FONT color="red"> No  </FONT> </TD> </TR>
-   * <TR><TD> TransformParams.Defined </TD> <TD> No </TD> </TR>
-   * <TR><TD> Junit </TD> <TD> Default </TD> </TR>
-   * <TR><TD> NewTests </TD> <TD> No </TD> </TR>
-   * <TR><TD> Seed.Defined </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> DefaultTupleSize.Defined </TD> <TD> No </TD> </TR>
    * </TABLE>
    * </P>
    */
   @Test
-  public void execute_6()
+  public void whenTransformDefMissing() throws Exception
     {
     // Given...
+    File baseDirTest = getBaseDirTest( "tcases-project-default");
 
+    TcasesMojo tcasesMojo = (TcasesMojo) mojoHelper.lookupConfiguredMojo( baseDirTest, "tcases");
+    tcasesMojo.setTransformDef( "Transform.xsl");
+
+    
     // When...
-
+    try
+      {
+      tcasesMojo.execute();
+      fail( "Expected failure missing");
+      }
     // Then...
+    catch( MojoExecutionException expected)
+      {
+      }
+    catch( Exception e)
+      {
+      throw new RuntimeException( "Unexpected failure", e);
+      }
     }
-
-  /**
-   * Tests {@link TcasesMojo#execute execute()} using the following inputs.
-   * <P>
-   * <TABLE border="1" cellpadding="8">
-   * <TR align="left"><TH colspan=2> 7. execute (<FONT color="red">Failure</FONT>) </TH></TR>
-   * <TR align="left"><TH> Input Choice </TH> <TH> Value </TH></TR>
-   * <TR><TD> InputDefPatterns.Count </TD> <TD> None </TD> </TR>
-   * <TR><TD> InputDefPatterns.Matched </TD> <TD> Many </TD> </TR>
-   * <TR><TD> InputDir </TD> <TD> Default </TD> </TR>
-   * <TR><TD> OutDir </TD> <TD> Default </TD> </TR>
-   * <TR><TD> OutFile.Defined </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> OutFile.Wildcard </TD> <TD> None </TD> </TR>
-   * <TR><TD> TestDef.Defined </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> TestDef.Wildcard </TD> <TD> None </TD> </TR>
-   * <TR><TD> TestDef.Exists </TD> <TD> <FONT color="red"> No  </FONT> </TD> </TR>
-   * <TR><TD> GenDef.Defined </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> GenDef.Wildcard </TD> <TD> None </TD> </TR>
-   * <TR><TD> GenDef.Exists </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> TransformDef.Defined </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> TransformDef.Relative </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> TransformDef.Wildcard </TD> <TD> None </TD> </TR>
-   * <TR><TD> TransformDef.Exists </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> TransformParams.Defined </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> Junit </TD> <TD> No </TD> </TR>
-   * <TR><TD> NewTests </TD> <TD> Default </TD> </TR>
-   * <TR><TD> Seed.Defined </TD> <TD> No </TD> </TR>
-   * <TR><TD> DefaultTupleSize.Defined </TD> <TD> No </TD> </TR>
-   * </TABLE>
-   * </P>
-   */
-  @Test
-  public void execute_7()
-    {
-    // Given...
-
-    // When...
-
-    // Then...
-    }
-
-  /**
-   * Tests {@link TcasesMojo#execute execute()} using the following inputs.
-   * <P>
-   * <TABLE border="1" cellpadding="8">
-   * <TR align="left"><TH colspan=2> 8. execute (<FONT color="red">Failure</FONT>) </TH></TR>
-   * <TR align="left"><TH> Input Choice </TH> <TH> Value </TH></TR>
-   * <TR><TD> InputDefPatterns.Count </TD> <TD> One </TD> </TR>
-   * <TR><TD> InputDefPatterns.Matched </TD> <TD> One </TD> </TR>
-   * <TR><TD> InputDir </TD> <TD> Default </TD> </TR>
-   * <TR><TD> OutDir </TD> <TD> Default </TD> </TR>
-   * <TR><TD> OutFile.Defined </TD> <TD> No </TD> </TR>
-   * <TR><TD> OutFile.Wildcard </TD> <TD> NA </TD> </TR>
-   * <TR><TD> TestDef.Defined </TD> <TD> No </TD> </TR>
-   * <TR><TD> TestDef.Wildcard </TD> <TD> NA </TD> </TR>
-   * <TR><TD> TestDef.Exists </TD> <TD> NA </TD> </TR>
-   * <TR><TD> GenDef.Defined </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> GenDef.Wildcard </TD> <TD> One </TD> </TR>
-   * <TR><TD> GenDef.Exists </TD> <TD> <FONT color="red"> No  </FONT> </TD> </TR>
-   * <TR><TD> TransformDef.Defined </TD> <TD> No </TD> </TR>
-   * <TR><TD> TransformDef.Relative </TD> <TD> NA </TD> </TR>
-   * <TR><TD> TransformDef.Wildcard </TD> <TD> NA </TD> </TR>
-   * <TR><TD> TransformDef.Exists </TD> <TD> NA </TD> </TR>
-   * <TR><TD> TransformParams.Defined </TD> <TD> No </TD> </TR>
-   * <TR><TD> Junit </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> NewTests </TD> <TD> No </TD> </TR>
-   * <TR><TD> Seed.Defined </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> DefaultTupleSize.Defined </TD> <TD> No </TD> </TR>
-   * </TABLE>
-   * </P>
-   */
-  @Test
-  public void execute_8()
-    {
-    // Given...
-
-    // When...
-
-    // Then...
-    }
-
-  /**
-   * Tests {@link TcasesMojo#execute execute()} using the following inputs.
-   * <P>
-   * <TABLE border="1" cellpadding="8">
-   * <TR align="left"><TH colspan=2> 9. execute (<FONT color="red">Failure</FONT>) </TH></TR>
-   * <TR align="left"><TH> Input Choice </TH> <TH> Value </TH></TR>
-   * <TR><TD> InputDefPatterns.Count </TD> <TD> Many </TD> </TR>
-   * <TR><TD> InputDefPatterns.Matched </TD> <TD> Many </TD> </TR>
-   * <TR><TD> InputDir </TD> <TD> Default </TD> </TR>
-   * <TR><TD> OutDir </TD> <TD> Default </TD> </TR>
-   * <TR><TD> OutFile.Defined </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> OutFile.Wildcard </TD> <TD> <FONT color="red"> Many  </FONT> </TD> </TR>
-   * <TR><TD> TestDef.Defined </TD> <TD> NA </TD> </TR>
-   * <TR><TD> TestDef.Wildcard </TD> <TD> NA </TD> </TR>
-   * <TR><TD> TestDef.Exists </TD> <TD> NA </TD> </TR>
-   * <TR><TD> GenDef.Defined </TD> <TD> No </TD> </TR>
-   * <TR><TD> GenDef.Wildcard </TD> <TD> NA </TD> </TR>
-   * <TR><TD> GenDef.Exists </TD> <TD> NA </TD> </TR>
-   * <TR><TD> TransformDef.Defined </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> TransformDef.Relative </TD> <TD> No </TD> </TR>
-   * <TR><TD> TransformDef.Wildcard </TD> <TD> One </TD> </TR>
-   * <TR><TD> TransformDef.Exists </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> TransformParams.Defined </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> Junit </TD> <TD> Default </TD> </TR>
-   * <TR><TD> NewTests </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> Seed.Defined </TD> <TD> No </TD> </TR>
-   * <TR><TD> DefaultTupleSize.Defined </TD> <TD> No </TD> </TR>
-   * </TABLE>
-   * </P>
-   */
-  @Test
-  public void execute_9()
-    {
-    // Given...
-
-    // When...
-
-    // Then...
-    }
-
-  /**
-   * Tests {@link TcasesMojo#execute execute()} using the following inputs.
-   * <P>
-   * <TABLE border="1" cellpadding="8">
-   * <TR align="left"><TH colspan=2> 10. execute (<FONT color="red">Failure</FONT>) </TH></TR>
-   * <TR align="left"><TH> Input Choice </TH> <TH> Value </TH></TR>
-   * <TR><TD> InputDefPatterns.Count </TD> <TD> None </TD> </TR>
-   * <TR><TD> InputDefPatterns.Matched </TD> <TD> One </TD> </TR>
-   * <TR><TD> InputDir </TD> <TD> Default </TD> </TR>
-   * <TR><TD> OutDir </TD> <TD> Default </TD> </TR>
-   * <TR><TD> OutFile.Defined </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> OutFile.Wildcard </TD> <TD> One </TD> </TR>
-   * <TR><TD> TestDef.Defined </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> TestDef.Wildcard </TD> <TD> One </TD> </TR>
-   * <TR><TD> TestDef.Exists </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> GenDef.Defined </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> GenDef.Wildcard </TD> <TD> <FONT color="red"> Many  </FONT> </TD> </TR>
-   * <TR><TD> GenDef.Exists </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> TransformDef.Defined </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> TransformDef.Relative </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> TransformDef.Wildcard </TD> <TD> None </TD> </TR>
-   * <TR><TD> TransformDef.Exists </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> TransformParams.Defined </TD> <TD> No </TD> </TR>
-   * <TR><TD> Junit </TD> <TD> No </TD> </TR>
-   * <TR><TD> NewTests </TD> <TD> Default </TD> </TR>
-   * <TR><TD> Seed.Defined </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> DefaultTupleSize.Defined </TD> <TD> No </TD> </TR>
-   * </TABLE>
-   * </P>
-   */
-  @Test
-  public void execute_10()
-    {
-    // Given...
-
-    // When...
-
-    // Then...
-    }
-
-  /**
-   * Tests {@link TcasesMojo#execute execute()} using the following inputs.
-   * <P>
-   * <TABLE border="1" cellpadding="8">
-   * <TR align="left"><TH colspan=2> 11. execute (<FONT color="red">Failure</FONT>) </TH></TR>
-   * <TR align="left"><TH> Input Choice </TH> <TH> Value </TH></TR>
-   * <TR><TD> InputDefPatterns.Count </TD> <TD> One </TD> </TR>
-   * <TR><TD> InputDefPatterns.Matched </TD> <TD> Many </TD> </TR>
-   * <TR><TD> InputDir </TD> <TD> Default </TD> </TR>
-   * <TR><TD> OutDir </TD> <TD> Default </TD> </TR>
-   * <TR><TD> OutFile.Defined </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> OutFile.Wildcard </TD> <TD> None </TD> </TR>
-   * <TR><TD> TestDef.Defined </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> TestDef.Wildcard </TD> <TD> None </TD> </TR>
-   * <TR><TD> TestDef.Exists </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> GenDef.Defined </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> GenDef.Wildcard </TD> <TD> None </TD> </TR>
-   * <TR><TD> GenDef.Exists </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> TransformDef.Defined </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> TransformDef.Relative </TD> <TD> No </TD> </TR>
-   * <TR><TD> TransformDef.Wildcard </TD> <TD> <FONT color="red"> Many  </FONT> </TD> </TR>
-   * <TR><TD> TransformDef.Exists </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> TransformParams.Defined </TD> <TD> Yes </TD> </TR>
-   * <TR><TD> Junit </TD> <TD> Default </TD> </TR>
-   * <TR><TD> NewTests </TD> <TD> No </TD> </TR>
-   * <TR><TD> Seed.Defined </TD> <TD> No </TD> </TR>
-   * <TR><TD> DefaultTupleSize.Defined </TD> <TD> No </TD> </TR>
-   * </TABLE>
-   * </P>
-   */
-  @Test
-  public void execute_11()
-    {
-    // Given...
-
-    // When...
-
-    // Then...
-    }
-
+  
   /**
    * Returns the set of paths relative to the given base directory matching any of the given patterns.
    */
