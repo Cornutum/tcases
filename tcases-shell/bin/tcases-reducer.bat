d8bc0480f6a46a15d6590dae9693d2483cfedb1b
Kerry Kimbrough <kerry.kimbrough@bazaarvoice.com>
2015-09-10 16:31:28 -0500

    tcases-reducer: Rework command line using -L option to change logging level and "-l stdout" to write logs to standard output.

--- tcases-shell/bin/tcases-reducer.bat
+++ tcases-shell/bin/tcases-reducer.bat
@@ -4,25 +4,34 @@ rem #
 rem #                     Copyright 2014, Cornutum Project
 rem #                              www.cornutum.org
 rem # 
-rem #                             $Revision$
-rem #          $Date$
-rem # 
 rem #######################################################################
 
 set TCASES_HOME=%~dp0..
 set TCASES_LIB=%TCASES_HOME%\lib
 set REDUCER_ARGS=
+set REDUCER_LOG_DEST=tcases.log.file
 set REDUCER_LOG=reducer.log
+set REDUCER_LOG_LEVEL=INFO
 
 :argsRead
 if "%1"=="" goto argsDone
 if "%1"=="-l" goto argsLogFile
+if "%1"=="-L" goto argsLogLevel
 set REDUCER_ARGS=%REDUCER_ARGS% %1
 goto argsNext
 
 :argsLogFile
 shift
-REDUCER_LOG=%1
+set REDUCER_LOG_DEST=tcases.log.file
+set REDUCER_LOG=%1
+if not "%1"=="stdout" goto argsNext
+set REDUCER_LOG_DEST=tcases.log.dest
+set REDUCER_LOG=STDOUT
+goto argsNext
+
+:argsLogLevel
+shift
+set REDUCER_LOG_LEVEL=%1
 goto argsNext
 
 :argsNext
@@ -40,4 +49,4 @@ set REDUCER_CP=%REDUCER_CP%;%1
 goto :eof
 
 :reducerRun
-java -cp "%REDUCER_CP%" -Dtcases.log.file=%REDUCER_LOG% org.cornutum.tcases.Reducer %REDUCER_ARGS%
+java -cp "%REDUCER_CP%" -D%REDUCER_LOG_DEST%=%REDUCER_LOG% -Dtcases.log.level=%REDUCER_LOG_LEVEL% org.cornutum.tcases.Reducer %REDUCER_ARGS%
