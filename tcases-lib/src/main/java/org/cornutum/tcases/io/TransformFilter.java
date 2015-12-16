f16d62f79d14d111ddf97646fc1fb08f6f7b9f68
Kerry Kimbrough <kerry.kimbrough@bazaarvoice.com>
2015-12-16 14:11:01 -0600

    TransformFilter: Refactor using base class=AbstractFilter

--- tcases-lib/src/main/java/org/cornutum/tcases/io/TransformFilter.java
+++ tcases-lib/src/main/java/org/cornutum/tcases/io/TransformFilter.java
@@ -9,17 +9,12 @@ package org.cornutum.tcases.io;
 
 import org.cornutum.tcases.util.ToString;
 
-import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
  
 import java.io.File;
-import java.io.FilterOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.PipedInputStream;
-import java.io.PipedOutputStream;
 import java.util.Map;
 import javax.xml.transform.ErrorListener;
 import javax.xml.transform.Result;
@@ -35,29 +30,8 @@ import javax.xml.transform.stream.StreamSource;
  * stream into results written to a {@link #getTarget target}.
  *
  */
-public class TransformFilter implements Runnable, ErrorListener
+public class TransformFilter extends AbstractFilter implements ErrorListener
   {
-  /**
-   * Receives the input data to be transformed.
-   *
-   */
-  private class TransformSourceStream extends FilterOutputStream
-    {
-    /**
-     * Creates a new TransformSourceStream object.
-     */
-    public TransformSourceStream( OutputStream source)
-      {
-      super( source);
-      }
-
-    public void close() throws IOException
-      {
-      super.close();
-      complete();
-      }
-    }
-
   /**
    * Creates a new TransformFilter object.
    */
@@ -67,7 +41,7 @@ public class TransformFilter implements Runnable, ErrorListener
     }
   
   /**
-   * Creates a new TransformFilter object.
+   * Creates a new TransformFilter to apply the given XSLT transform.
    */
   public TransformFilter( Source transform)
     {
@@ -75,7 +49,7 @@ public class TransformFilter implements Runnable, ErrorListener
     }
   
   /**
-   * Creates a new TransformFilter object.
+   * Creates a new TransformFilter to apply the given XSLT transform.
    */
   public TransformFilter( InputStream transform)
     {
@@ -83,7 +57,7 @@ public class TransformFilter implements Runnable, ErrorListener
     }
   
   /**
-   * Creates a new TransformFilter object.
+   * Creates a new TransformFilter to apply the given XSLT transform.
    */
   public TransformFilter( File transform)
     {
@@ -124,73 +98,9 @@ public class TransformFilter implements Runnable, ErrorListener
     }
 
   /**
-   * Changes the target for transformed output.
+   * Initializes the filter.
    */
-  public void setTarget( File target)
-    {
-    Result result = target==null? null : new StreamResult( target);
-    setTarget( result);
-    }
-
-  /**
-   * Changes the target for transformed output.
-   */
-  public void setTarget( OutputStream target)
-    {
-    Result result = target==null? null : new StreamResult( target);
-    setTarget( result);
-    }
-
-  /**
-   * Changes the target for transformed output.
-   */
-  public void setTarget( Result target)
-    {
-    close();
-    target_ = target;
-    }
-
-  /**
-   * Returns the target for transformed output.
-   */
-  public Result getTarget()
-    {
-    return target_;
-    }
-
-  /**
-   * Returns the target identifier.
-   */
-  public String getTargetId()
-    {
-    String systemId;
-    return
-      target_ == null?
-      null :
-
-      (systemId = target_.getSystemId()) == null?
-      "?" :
-
-      systemId;
-    }
-
-  /**
-   * Returns the source for transform input.
-   */
-  public OutputStream getSource()
-    {
-    if( source_ == null)
-      {
-      start();
-      }
-    
-    return source_;
-    }
-
-  /**
-   * Starts transform output.
-   */
-  private void start()
+  protected void initializeFilter( InputStream filterInput, OutputStream filterOutput)
     {
     try
       {
@@ -212,138 +122,52 @@ public class TransformFilter implements Runnable, ErrorListener
           transformer_.setParameter( paramName, transformParams_.get( paramName));
           }
         }
-
-      // Create filter streams.
-      PipedOutputStream pipeOut = new PipedOutputStream();
-      PipedInputStream pipeIn = new PipedInputStream( pipeOut);
-      input_ = new StreamSource( pipeIn);
-      source_ = new TransformSourceStream( pipeOut);
-
-      // Start filter thread.
-      failure_ = null;
-      thread_ = new Thread( this, String.valueOf( this));
-      thread_.start();
+      
+      transformSource_ = new StreamSource( filterInput);
+      transformResult_ = new StreamResult( filterOutput);
       }
     catch( Exception e)
       {
-      throw new RuntimeException( "Can't start filtering", e);
+      throw new RuntimeException( "Can't initialize filter", e);
       } 
     }
 
   /**
-   * Terminates transform output.
+   * Reads data to be transformed from the {@link #getFilterInput filter input stream} and
+   * write transformed data to the {@link #getFilterOutput filter output stream}.
    */
-  public void close()
+  protected void applyFilter() throws Exception
     {
-    IOUtils.closeQuietly( source_);
-    try
-      {
-      complete();
-      }
-    catch( Exception e)
-      {
-      logger_.error( "Can't complete transform", e);
-      }
-    }
-
-  /**
-   * Completes transform output.
-   */
-  private void complete() throws IOException
-    {
-    if( source_ != null)
-      {
-      try
-        {
-        thread_.join();
-        }
-      catch( Exception e)
-        {
-        logger_.error( "Thread=" + thread_ + " not completed", e);
-        }
-
-      IOException failure =
-        failure_ == null
-        ? null
-        : new IOException( "Can't write output to target=" + getTargetId(), failure_);
-      
-      failure_ = null;
-      input_ = null;
-      source_ = null;
-      target_ = null;
-      thread_ = null;
-      transform_ = null;
-      transformer_ = null;
-
-      if( failure != null)
-        {
-        throw failure;
-        }
-      }
-    }  
-
-  public void run()
-    {
-    logger_.debug( "Starting, thread={}", thread_);
-    
-    try
-      {
-      // If no target defined, write to standard output.
-      Result target =
-        target_ == null
-        ? new StreamResult( System.out)
-        : target_;
-      
-      transformer_.transform( input_, target);
-      }
-    catch( Exception e)
-      {
-      failure_ = e;
-      logger_.error( "Can't complete transform", e);
-      }
-    finally
-      {
-      OutputStream targetStream =
-        target_ instanceof StreamResult
-        ? ((StreamResult) target_).getOutputStream()
-        : null;
-
-      IOUtils.closeQuietly( targetStream);
-      }
-    
-    logger_.debug( "Completed, thread={}", thread_);
+    transformer_.transform( transformSource_, transformResult_);
     }
 
   public void error( TransformerException exception) throws TransformerException
     {
-    logger_.error( "target={}, {}", getTargetId(), exception);
+    logger_.error( "Exception during transform, {}", exception);
     throw exception;
     }
   
   public void fatalError( TransformerException exception) throws TransformerException
     {
-    logger_.error( "target={}, {}", getTargetId(), exception);
+    logger_.error( "Exception during transform, {}", exception);
     throw exception;
     }
   
  public void warning( TransformerException exception)
     {
-    logger_.warn( "target={}, {}", getTargetId(), exception);
+    logger_.warn( "Exception during transform, {}", exception);
     }
 
   public String toString()
     {
     return
       ToString.getBuilder( this)
-      .append( "target", getTargetId())
+      .append( "transform", transform_==null? null : transform_.getSystemId())
       .toString();
     }
   
-  private Exception failure_;
-  private StreamSource input_;
-  private OutputStream source_;
-  private Result target_;
-  private Thread thread_;
+  private StreamSource transformSource_;
+  private Result transformResult_;
   private Source transform_;
   private Transformer transformer_;
   private Map<String,Object> transformParams_;
