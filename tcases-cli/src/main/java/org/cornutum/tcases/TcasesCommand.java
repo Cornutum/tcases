3e7a7226f1fcd10f300f132a397a8beeeb0f7415
Kerry Kimbrough <kerrykimbrough@gmail.com>
2019-02-14 13:16:17 -0600

    FilenameUtils is now obsolete

--- tcases-cli/src/main/java/org/cornutum/tcases/TcasesCommand.java
+++ tcases-cli/src/main/java/org/cornutum/tcases/TcasesCommand.java
@@ -11,8 +11,8 @@ import org.apache.commons.lang3.StringUtils;
 import org.cornutum.tcases.generator.*;
 import org.cornutum.tcases.generator.io.*;
 import org.cornutum.tcases.io.*;
-import org.cornutum.tcases.util.FilenameUtils;
 
+import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
