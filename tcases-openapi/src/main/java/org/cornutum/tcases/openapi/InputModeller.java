a92957614bb8567c068d3a01d4eba009b8cddfb6
Kerry Kimbrough <kerrykimbrough@gmail.com>
2020-05-02 14:14:35 -0500

    InputModeller: uniqueItems property must be available via annotation for failure cases where Unique var is NA.

--- tcases-openapi/src/main/java/org/cornutum/tcases/openapi/InputModeller.java
+++ tcases-openapi/src/main/java/org/cornutum/tcases/openapi/InputModeller.java
@@ -1131,6 +1131,12 @@ public abstract class InputModeller extends ConditionReporter<OpenApiContext>
       size.hasIf( "itemMinItems", minItems);
       size.hasIf( "itemMaxItems", maxItems);
       }
+
+    boolean uniqueRequired = Boolean.TRUE.equals( arraySchema.getUniqueItems());
+    if( uniqueRequired)
+      {
+      size.has( "itemsUnique", true);
+      }
     
     return size.build();
     }
