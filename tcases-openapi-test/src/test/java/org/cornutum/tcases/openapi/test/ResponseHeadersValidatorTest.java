a1706be734f0c3c46ec33da022abea41edfb3da9
Kerry Kimbrough <kerrykimbrough@gmail.com>
2022-04-02 19:09:51 -0500

    ResponseHeadersValidatorTest: Complete tests for text/plain, application/json

--- tcases-openapi-test/src/test/java/org/cornutum/tcases/openapi/test/ResponseHeadersValidatorTest.java
+++ tcases-openapi-test/src/test/java/org/cornutum/tcases/openapi/test/ResponseHeadersValidatorTest.java
@@ -20,7 +20,7 @@ import java.util.Map;
 public class ResponseHeadersValidatorTest extends ResponseValidatorTest
   {
   @Test
-  public void whenContentJsonArray()
+  public void whenJsonArray()
     {
     // Given...
     ResponseValidator validator = validatorFor( "responsesDef-headers", UNVALIDATED_FAIL);
@@ -84,6 +84,7 @@ public class ResponseHeadersValidatorTest extends ResponseValidatorTest
       .put( "My-String", "\"A,B,C,D\"")
       .put( "My-Simple-Array", "1,2,3,")
       .put( "My-Mixed-Array", "1,,-2.0,")
+      .put( "My-Number-Array", "-123.0")
       .build();
     
     // Then...
@@ -168,12 +169,65 @@ public class ResponseHeadersValidatorTest extends ResponseValidatorTest
     
         // Then...
         validator.assertHeadersValid( op, path, statusCode, headers);
+        });
+
+    // Then...
+    expectFailure( ResponseValidationException.class)
+      .when( () -> {
+        Map<String,String> headers =
+          headers()
+          .put( "My-Simple-Array", "")
+          .put( "My-Number-Array", "A,B,C")
+          .build();
+    
+        // Then...
+        validator.assertHeadersValid( op, path, statusCode, headers);
         })
       .then( failure -> {
         assertValidationErrors(
           failure,
-          "get /responses (200), My-Mixed-Array: invalid value",
-          "#minItems: Min items is '1', found '0'.");
+          "get /responses (200), My-Number-Array: invalid value",
+          "0#items/type: Type expected 'number', found 'string'.",
+          "1#items/type: Type expected 'number', found 'string'.",
+          "2#items/type: Type expected 'number', found 'string'.");
+        });
+
+    // Then...
+    expectFailure( ResponseValidationException.class)
+      .when( () -> {
+        Map<String,String> headers =
+          headers()
+          .put( "My-Simple-Array", "")
+          .put( "My-Number-Array", "3,9,")
+          .build();
+    
+        // Then...
+        validator.assertHeadersValid( op, path, statusCode, headers);
+        })
+      .then( failure -> {
+        assertValidationErrors(
+          failure,
+          "get /responses (200), My-Number-Array: invalid value",
+          "2#items/type: Type expected 'number', found 'string'.");
+        });
+
+    // Then...
+    expectFailure( ResponseValidationException.class)
+      .when( () -> {
+        Map<String,String> headers =
+          headers()
+          .put( "My-Simple-Array", "")
+          .put( "My-Number-Array", "3,9,11")
+          .build();
+    
+        // Then...
+        validator.assertHeadersValid( op, path, statusCode, headers);
+        })
+      .then( failure -> {
+        assertValidationErrors(
+          failure,
+          "get /responses (200), My-Number-Array: invalid value",
+          "2#items/multipleOf: Value '11' is not a multiple of '3'.");
         });
     }
 
@@ -276,6 +330,438 @@ public class ResponseHeadersValidatorTest extends ResponseValidatorTest
           "patch /responses (200), My-Simple-Object: invalid value",
           "#type: Type expected 'object', found 'array'.",
           "#required: Field 'A' is required.");
+        });
+    }
+  
+  @Test
+  public void whenSimpleInteger()
+    {
+    // Given...
+    ResponseValidator validator = validatorFor( "responsesDef-headers", UNVALIDATED_FAIL);
+    String op = "delete";
+    String path = "/responses";
+    int statusCode = 400;
+
+    {
+    // When...
+    Map<String,String> headers =
+      headers()
+      .put( "My-Simple-Integer", "0")
+      .build();
+    
+    // Then...
+    validator.assertHeadersValid( op, path, statusCode, headers);
+    }
+    {
+    // When...
+    Map<String,String> headers =
+      headers()
+      .build();
+    
+    // Then...
+    validator.assertHeadersValid( op, path, statusCode, headers);
+    }
+
+    // Then...
+    expectFailure( ResponseValidationException.class)
+      .when( () -> {
+        Map<String,String> headers =
+          headers()
+          .put( "My-Simple-Integer", "")
+          .build();
+    
+        // Then...
+        validator.assertHeadersValid( op, path, statusCode, headers);
+        })
+      .then( failure -> {
+        assertValidationErrors(
+          failure,
+          "delete /responses (400), My-Simple-Integer: invalid value",
+          "#nullable: Null value is not allowed.");
+        });
+
+    // Then...
+    expectFailure( ResponseValidationException.class)
+      .when( () -> {
+        Map<String,String> headers =
+          headers()
+          .put( "My-Simple-Integer", "-1,0,1")
+          .build();
+    
+        // Then...
+        validator.assertHeadersValid( op, path, statusCode, headers);
+        })
+      .then( failure -> {
+        assertValidationErrors(
+          failure,
+          "delete /responses (400), My-Simple-Integer: invalid value",
+          "#type: Type expected 'integer', found 'array'.");
+        });
+
+    // Then...
+    expectFailure( ResponseValidationException.class)
+      .when( () -> {
+        Map<String,String> headers =
+          headers()
+          .put( "My-Simple-Integer", "-101")
+          .build();
+    
+        // Then...
+        validator.assertHeadersValid( op, path, statusCode, headers);
+        })
+      .then( failure -> {
+        assertValidationErrors(
+          failure,
+          "delete /responses (400), My-Simple-Integer: invalid value",
+          "#minimum: Minimum is '-1', found '-101'.");
+        });
+    }
+  
+  @Test
+  public void whenUnexplodedObject()
+    {
+    // Given...
+    ResponseValidator validator = validatorFor( "responsesDef-headers", UNVALIDATED_FAIL);
+    String op = "patch";
+    String path = "/responses";
+    int statusCode = 400;
+
+    {
+    // When...
+    Map<String,String> headers =
+      headers()
+      .put( "My-Simple-Object", "A,true,B,3.12,C,Hello")
+      .build();
+    
+    // Then...
+    validator.assertHeadersValid( op, path, statusCode, headers);
+    }
+    {
+    // When...
+    Map<String,String> headers =
+      headers()
+      .put( "My-Simple-Object", "A,false,B,,C,3.12,D,?")
+      .build();
+    
+    // Then...
+    validator.assertHeadersValid( op, path, statusCode, headers);
+    }
+    {
+    // When...
+    Map<String,String> headers =
+      headers()
+      .put( "My-Simple-Object", "")
+      .build();
+    
+    // Then...
+    validator.assertHeadersValid( op, path, statusCode, headers);
+    }
+    {
+    // When...
+    Map<String,String> headers =
+      headers()
+      .put( "My-Simple-Object", "B=X,")
+      .build();
+    
+    // Then...
+    validator.assertHeadersValid( op, path, statusCode, headers);
+    }
+    {
+    // When...
+    Map<String,String> headers =
+      headers()
+      .put( "My-Simple-Object", "C,")
+      .build();
+    
+    // Then...
+    validator.assertHeadersValid( op, path, statusCode, headers);
+    }
+
+    // Then...
+    expectFailure( ResponseValidationException.class)
+      .when( () -> {
+        Map<String,String> headers =
+          headers()
+          .put( "My-Simple-Object", "C,Hello,A")
+          .build();
+    
+        // Then...
+        validator.assertHeadersValid( op, path, statusCode, headers);
+        })
+      .then( failure -> {
+        assertValidationErrors(
+          failure,
+          "patch /responses (400), My-Simple-Object: invalid value",
+          "#type: Type expected 'object', found 'array'.");
+        });
+
+    // Then...
+    expectFailure( ResponseValidationException.class)
+      .when( () -> {
+        Map<String,String> headers =
+          headers()
+          .put( "My-Simple-Object", "A,")
+          .build();
+    
+        // Then...
+        validator.assertHeadersValid( op, path, statusCode, headers);
+        })
+      .then( failure -> {
+        assertValidationErrors(
+          failure,
+          "patch /responses (400), My-Simple-Object: invalid value",
+          "A#type: Type expected 'boolean', found 'string'.");
+        });
+    }
+  
+  @Test
+  public void whenSimpleString()
+    {
+    // Given...
+    ResponseValidator validator = validatorFor( "responsesDef-headers", UNVALIDATED_FAIL);
+    String op = "get";
+    String path = "/responses";
+    int statusCode = 400;
+
+    {
+    // When...
+    Map<String,String> headers =
+      headers()
+      .put( "My-Simple-String", "123")
+      .build();
+    
+    // Then...
+    validator.assertHeadersValid( op, path, statusCode, headers);
+    }
+    {
+    // When...
+    Map<String,String> headers =
+      headers()
+      .put( "My-Simple-String", "")
+      .build();
+    
+    // Then...
+    validator.assertHeadersValid( op, path, statusCode, headers);
+    }
+    {
+    // When...
+    Map<String,String> headers =
+      headers()
+      .put( "My-Simple-String", "A,B,C")
+      .build();
+    
+    // Then...
+    validator.assertHeadersValid( op, path, statusCode, headers);
+    }
+    }
+  
+  @Test
+  public void whenJsonString()
+    {
+    // Given...
+    ResponseValidator validator = validatorFor( "responsesDef-headers", UNVALIDATED_FAIL);
+    String op = "post";
+    String path = "/responses";
+    int statusCode = 200;
+
+    {
+    // When...
+    Map<String,String> headers =
+      headers()
+      .put( "My-Json-String", "\"Hello\"")
+      .build();
+    
+    // Then...
+    validator.assertHeadersValid( op, path, statusCode, headers);
+    }
+    {
+    // When...
+    Map<String,String> headers =
+      headers()
+      .build();
+    
+    // Then...
+    validator.assertHeadersValid( op, path, statusCode, headers);
+    }
+
+    // Then...
+    expectFailure( ResponseValidationException.class)
+      .when( () -> {
+        Map<String,String> headers =
+          headers()
+          .put( "My-Json-String", "")
+          .build();
+    
+        // Then...
+        validator.assertHeadersValid( op, path, statusCode, headers);
+        })
+      .then( failure -> {
+        assertValidationErrors(
+          failure,
+          "post /responses (200), My-Json-String: Can't decode as contentType=application/json",
+          "No JSON content found");
+        });
+
+    // Then...
+    expectFailure( ResponseValidationException.class)
+      .when( () -> {
+        Map<String,String> headers =
+          headers()
+          .put( "My-Json-String", "Hello")
+          .build();
+    
+        // Then...
+        validator.assertHeadersValid( op, path, statusCode, headers);
+        })
+      .then( failure -> {
+        assertValidationErrors(
+          failure,
+          "post /responses (200), My-Json-String: Can't decode as contentType=application/json",
+          "Unrecognized token 'Hello': was expecting (JSON String, Number, Array, Object or token 'null', 'true' or 'false')",
+          " at [Source: (StringReader); line: 1, column: 6]");
+        });
+
+    // Then...
+    expectFailure( ResponseValidationException.class)
+      .when( () -> {
+        Map<String,String> headers =
+          headers()
+          .put( "My-Json-String", "null")
+          .build();
+    
+        // Then...
+        validator.assertHeadersValid( op, path, statusCode, headers);
+        })
+      .then( failure -> {
+        assertValidationErrors(
+          failure,
+          "post /responses (200), My-Json-String: invalid value",
+          "#nullable: Null value is not allowed.");
+        });
+    }
+  
+  @Test
+  public void whenJsonInteger()
+    {
+    // Given...
+    ResponseValidator validator = validatorFor( "responsesDef-headers", UNVALIDATED_FAIL);
+    String op = "post";
+    String path = "/responses";
+    int statusCode = 200;
+
+    {
+    // When...
+    Map<String,String> headers =
+      headers()
+      .put( "My-Json-Integer", "111222333444")
+      .build();
+    
+    // Then...
+    validator.assertHeadersValid( op, path, statusCode, headers);
+    }
+
+    // Then...
+    expectFailure( ResponseValidationException.class)
+      .when( () -> {
+        Map<String,String> headers =
+          headers()
+          .put( "My-Json-Integer", "111222.333444")
+          .build();
+    
+        // Then...
+        validator.assertHeadersValid( op, path, statusCode, headers);
+        })
+      .then( failure -> {
+        assertValidationErrors(
+          failure,
+          "post /responses (200), My-Json-Integer: invalid value",
+          "#type: Type expected 'integer', found 'number'.");
+        });
+
+    // Then...
+    expectFailure( ResponseValidationException.class)
+      .when( () -> {
+        Map<String,String> headers =
+          headers()
+          .put( "My-Json-Integer", "123/456")
+          .build();
+    
+        // Then...
+        validator.assertHeadersValid( op, path, statusCode, headers);
+        })
+      .then( failure -> {
+        assertValidationErrors(
+          failure,
+          "post /responses (200), My-Json-Integer: Can't decode as contentType=application/json",
+          "Unexpected character ('/' (code 47)): Expected space separating root-level values",
+          " at [Source: (StringReader); line: 1, column: 5]");
+        });
+    }
+  
+  @Test
+  public void whenJsonObject()
+    {
+    // Given...
+    ResponseValidator validator = validatorFor( "responsesDef-headers", UNVALIDATED_FAIL);
+    String op = "post";
+    String path = "/responses";
+    int statusCode = 200;
+
+    {
+    // When...
+    Map<String,String> headers =
+      headers()
+      .put( "My-Json-Object", "{ \"A\": 0 }")
+      .build();
+    
+    // Then...
+    validator.assertHeadersValid( op, path, statusCode, headers);
+    }
+    {
+    // When...
+    Map<String,String> headers =
+      headers()
+      .put( "My-Json-Object", "null")
+      .build();
+    
+    // Then...
+    validator.assertHeadersValid( op, path, statusCode, headers);
+    }
+
+    // Then...
+    expectFailure( ResponseValidationException.class)
+      .when( () -> {
+        Map<String,String> headers =
+          headers()
+          .put( "My-Json-Object", "{ \"A\": 0, \"B\": \"1\" }")
+          .build();
+    
+        // Then...
+        validator.assertHeadersValid( op, path, statusCode, headers);
+        })
+      .then( failure -> {
+        assertValidationErrors(
+          failure,
+          "post /responses (200), My-Json-Object: invalid value",
+          "#maxProperties: Maximum is '1', found '2'.");
+        });
+
+    // Then...
+    expectFailure( ResponseValidationException.class)
+      .when( () -> {
+        Map<String,String> headers =
+          headers()
+          .put( "My-Json-Object", "[]")
+          .build();
+    
+        // Then...
+        validator.assertHeadersValid( op, path, statusCode, headers);
+        })
+      .then( failure -> {
+        assertValidationErrors(
+          failure,
+          "post /responses (200), My-Json-Object: invalid value",
+          "#type: Type expected 'object', found 'array'.");
         });
     }
 
