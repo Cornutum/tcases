2b9fe03eb4a3b0379377f320df15e2a51285ce65
Kerry Kimbrough <kerrykimbrough@gmail.com>
2022-04-03 19:00:51 -0500

    Add FormUrlDecoder

---
+++ tcases-openapi-test/src/test/java/org/cornutum/tcases/openapi/test/FormUrlBuilder.java
@@ -0,0 +1,67 @@
+package org.cornutum.tcases.openapi.test;
+
+import java.net.URLEncoder;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import static java.util.stream.Collectors.joining;
+
+/**
+ * Builds an application/x-www-form-urlencoded string.
+ */
+public class FormUrlBuilder
+  {
+  /**
+   * Creates a new FormUrlBuilder instance.
+   */
+  private FormUrlBuilder()
+    {
+    }
+
+  /**
+   * Returns a new FormUrlBuilder.
+   */
+  public static FormUrlBuilder with()
+    {
+    return new FormUrlBuilder();
+    }
+
+  /**
+   * Adds a field value to this form.
+   */
+  public FormUrlBuilder field( String name, String value)
+    {
+    fields_.put( encodeUrl( name), encodeUrl( value));
+    return this;
+    }
+
+  /**
+   * Returns the encoded form string.
+   */
+  public String build()
+    {
+    return
+      fields_.entrySet().stream()
+      .map( field -> String.format( "%s=%s", field.getKey(), field.getValue()))
+      .collect( joining( "&"));
+    }
+
+  /**
+   * Enccodes an application/x-www-form-urlencoded string.
+   */
+  protected String encodeUrl( String content)
+    {
+    try
+      {
+      return
+        content == null
+        ? null
+        : URLEncoder.encode( content, "UTF-8");
+      }
+    catch( Exception e)
+      {
+      throw new IllegalArgumentException( String.format( "Can't encode application/x-www-form-urlencoded content=%s", content), e);
+      }
+    }
+
+  private Map<String,String> fields_ = new LinkedHashMap<String,String>();
+  }
