890ae50b1644dd17067ca5c7378e4f6d6c4de34f
Kerry Kimbrough <kerrykimbrough@gmail.com>
2022-12-18 21:58:30 -0600

    Fix Javadoc

--- tcases-lib/src/main/java/org/cornutum/tcases/AnnotatedBuilder.java
+++ tcases-lib/src/main/java/org/cornutum/tcases/AnnotatedBuilder.java
@@ -93,7 +93,7 @@ public abstract class AnnotatedBuilder<T extends AnnotatedBuilder<T>>
     }
 
   /**
-   * Adds an annotation if the given value is non-null
+   * Adds annotations from another annotated element.
    */
   @SuppressWarnings("unchecked")
   public T annotations( Annotated other)
