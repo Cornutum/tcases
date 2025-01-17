9e643a93ce6adf85045bd8834d8a8ada11803a0f
Kerry Kimbrough <kerrykimbrough@gmail.com>
2025-01-17 14:35:52 -0600

    Update expected results for date/time schema tests

--- tcases-lib/src/test/java/org/cornutum/tcases/resolve/TestCaseSchemaResolverTest.java
+++ tcases-lib/src/test/java/org/cornutum/tcases/resolve/TestCaseSchemaResolverTest.java
@@ -236,7 +236,7 @@ public class TestCaseSchemaResolverTest extends ResolverTest
         .name( "Parent.Var='String'")
         .bind(
           VarBindingBuilder.with( "Parent.Var")
-          .value( "2023-08-29T16:05:42.931-05:00")
+          .value( "2024-08-28T16:05:42.931-05:00")
           .source( "String")
           .has( "format", "date-time")
           .build())
@@ -493,7 +493,7 @@ public class TestCaseSchemaResolverTest extends ResolverTest
         .name( "Parent.Var='String'")
         .bind(
           VarBindingBuilder.with( "Parent.Var")
-          .value( "2023-07-05")
+          .value( "2024-07-04")
           .source( "String")
           .has( "format", "date")
           .build())
@@ -584,7 +584,7 @@ public class TestCaseSchemaResolverTest extends ResolverTest
         .name( "Var='String'")
         .bind(
           VarBindingBuilder.with( "Var")
-          .value( "2023-08-29T16:05:42.931-05:00")
+          .value( "2024-08-28T16:05:42.931-05:00")
           .source( "String")
           .has( "format", "date-time")
           .build())
