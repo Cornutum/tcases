76c59375bbeadb00363143b9b5706b78c8f03048
Kerry Kimbrough <kerrykimbrough@gmail.com>
2025-02-19 18:55:57 -0600

    AnnotationFinder: Refactor using Files.classPathFor()

--- tcases-lib/src/main/java/org/cornutum/tcases/annotations/AnnotationFinder.java
+++ tcases-lib/src/main/java/org/cornutum/tcases/annotations/AnnotationFinder.java
@@ -10,21 +10,17 @@ package org.cornutum.tcases.annotations;
 import org.cornutum.annotation.Annotated;
 import org.cornutum.annotation.Finder;
 import org.cornutum.annotation.PackageFilter;
+import static org.cornutum.annotation.Files.classPathFor;
 
 import java.io.File;
 import java.lang.annotation.Annotation;
-import java.net.JarURLConnection;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.LinkedHashSet;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Stream;
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toCollection;
 
 /**
  * Finds annotated classes at runtime.
@@ -70,7 +66,7 @@ public class AnnotationFinder
    */
   public AnnotationFinder inPackage( Collection<String> packageNames)
     {
-    return inClasses( packageClasses( packageNames));
+    return inClasses( classPathFor( packageNames));
     }
 
   /**
@@ -151,103 +147,6 @@ public class AnnotationFinder
     catch( Exception e)
       {
       throw new IllegalStateException( String.format( "Can't load class=%s", className), e);
-      }
-    }
-
-  /**
-   * Returns the class path elements that contain the given packages
-   */
-  private Set<File> packageClasses( Collection<String> packageNames)
-    {
-    return
-      packageNames
-      .stream()
-
-      .flatMap( packageName -> {
-        try
-          {
-          return
-            Collections.list(
-              Thread.currentThread().getContextClassLoader().getResources(
-                Arrays.asList( packageName.split( "\\."))
-                .stream()
-                .collect( joining( "/", "", "/"))))
-            .stream();
-          }
-        catch( Exception e)
-          {
-          throw new IllegalArgumentException( String.format( "Can't get resources for package=%s", packageName), e);
-          }
-        })
-
-      .map( url -> {
-        Optional<File> classPathFile;
-        classPathFile =
-          "file".equals( url.getProtocol())?
-          toClassFile( url) :
-          
-          "jar".equals( url.getProtocol())?
-          toJarFile( url) :
-
-          Optional.empty();
-
-        return classPathFile;
-        })
-
-      .filter( Optional::isPresent)
-      .map( Optional::get)
-
-      .collect( toCollection( LinkedHashSet::new));
-    }
-
-  /**
-   * If the given URL represents a class file or directory, returns the file path.
-   */
-  private Optional<File> toClassFile( URL url)
-    {
-    try
-      {
-      return
-        Optional.of( toFile( url))
-        .filter( file -> file.isDirectory() || file.getPath().endsWith( ".class"));
-      }
-    catch( Exception e)
-      {
-      throw new IllegalStateException( String.format( "Can't get file from url=%s", url), e);
-      }
-    }
-
-  /**
-   * If the given URL represents a JAR file, returns the file path.
-   */
-  private Optional<File> toJarFile( URL url)
-    {
-    try
-      {
-      JarURLConnection jar = (JarURLConnection)url.openConnection();
-      return
-        Optional.of( jar.getJarFileURL())
-        .filter( jarUrl -> "file".equals( jarUrl.getProtocol()))
-        .map( jarUrl -> toFile( jarUrl));
-      }
-    catch( Exception e)
-      {
-      throw new IllegalStateException( String.format( "Can't get JAR file from url=%s", url), e);
-      }
-    }
-
-  /**
-   * Returns the file represented by the given URL:
-   */
-  private File toFile( URL url)
-    {
-    try
-      {
-      return new File( url.toURI().getPath());
-      }
-    catch( Exception e)
-      {
-      throw new IllegalStateException( String.format( "Can't get file from url=%s", url), e);
       }
     }
 
